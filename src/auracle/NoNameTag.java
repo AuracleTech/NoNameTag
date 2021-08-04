d4a84148cfc275fcbf01f7ec3faa088e57eae6c1
Auracle <VirgilFabre@outlook.com>
2021-08-04 13:58:52 -0400

    Hotfix

--- src/auracle/NoNameTag.java
+++ src/auracle/NoNameTag.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.scoreboard.Team;
@@ -17,33 +16,35 @@ public class NoNameTag extends JavaPlugin implements Listener {
     static Server server;
     static ConsoleCommandSender console;
     ScoreboardManager sbm;
-    Team NoNameTagTeam;
+    String NoTagTeamName = "NoNameTagTeam";
 
     public void onEnable() {
         server = this.getServer();
         server.getPluginManager().registerEvents(this, this);
         sbm = server.getScoreboardManager();
         console = server.getConsoleSender();
-        console.sendMessage(ChatColor.GREEN + "" + ChatColor.ITALIC + "NoNameTag Loaded");
-        for(Player player : server.getOnlinePlayers())
-            removeNameTag(player.getDisplayName());
+        console.sendMessage(ChatColor.GREEN + "" + ChatColor.ITALIC + "No Name Tag Loaded");
+        removeNameTags();
     }
 
-    public void onDisable() { console.sendMessage(ChatColor.RED + "NoNameTag Disabled"); }
+    public void onDisable() {
+        console.sendMessage(ChatColor.RED + "No Name Tag Disabled");
+    }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) { removeNameTag(event.getPlayer().getDisplayName()); }
-
-    private void removeNameTag(String PlayerName) {
-        if (!teamExists()) makeTeam();
-        NoNameTagTeam.addEntry(PlayerName);
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        removeNameTags();
     }
 
-    public boolean teamExists() { return sbm.getMainScoreboard().getTeam("NoNameTagTeam") != null; }
-
-    public void makeTeam() {
-        sbm.getMainScoreboard().registerNewTeam("NoNameTagTeam");
-        NoNameTagTeam = sbm.getMainScoreboard().getTeam("NoNameTagTeam");
-        NoNameTagTeam.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
+    private void removeNameTags() {
+        Team NoNameTagTeam = sbm.getMainScoreboard().getTeam(NoTagTeamName);
+        if (NoNameTagTeam == null) {
+            sbm.getMainScoreboard().registerNewTeam(NoTagTeamName);
+            NoNameTagTeam = sbm.getMainScoreboard().getTeam(NoTagTeamName);
+            NoNameTagTeam.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.NEVER);
+        }
+        NoNameTagTeam = sbm.getMainScoreboard().getTeam(NoTagTeamName);
+        for (Player player : server.getOnlinePlayers())
+            NoNameTagTeam.addEntry(player.getDisplayName());
     }
 }
